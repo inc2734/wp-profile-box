7d5b1c0420e04fc5fd637705a3849525666322f0
Takashi Kitajima <inc@2inc.org>
2019-08-07 14:36:46 +0900

    https://github.com/inc2734/snow-monkey/issues/603

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -63,6 +63,7 @@ class Bootstrap {
 			'wordpress' => __( 'WordPress', 'inc2734-wp-profile-box' ),
 			'tumblr'    => __( 'Tumblr', 'inc2734-wp-profile-box' ),
 			'amazon'    => __( 'Amazon', 'inc2734-wp-profile-box' ),
+			'line'      => __( 'LINE', 'inc2734-wp-profile-box' ),
 		] );
 
 		return $user_contactmethods;
